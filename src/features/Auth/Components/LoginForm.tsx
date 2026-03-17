989fbf32a0f64afa406aa3ca9b32c19cb1c06a0f
misael <missaelapaza@gmail.com>
2026-03-17 16:16:06 -0400

    login released

--- src/features/Auth/Components/LoginForm.tsx
+++ src/features/Auth/Components/LoginForm.tsx
@@ -1,94 +1,67 @@
-import { LoginSchema, Login } from '../schema/AuthSchema'
-import { zodResolver } from "@hookform/resolvers/zod"
-import { Controller, useForm } from "react-hook-form"
-import { Field, FieldError, FieldGroup, FieldLabel } from '@/components/ui/field'
-import { Input } from '@/components/ui/input'
-import { useLoginUser } from '../hooks/AuthHooks'
-import { Card, CardContent, CardDescription, CardFooter, CardHeader, CardTitle } from '@/components/ui/card'
-import { Button } from '@/components/ui/button'
+import { LoginSchema, Login } from "../schema/AuthSchema";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { Field, FieldGroup } from "@/components/ui/field";
+import { useLoginUser } from "../hooks/AuthHooks";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Button } from "@/components/ui/button";
+import { FormInput } from "@/components/form/FormInput";
 
 function LoginForm() {
-    const { mutate: LoginUser } = useLoginUser();
+  const { mutate: LoginUser } = useLoginUser();
 
-    const form = useForm<Login>({
-        resolver: zodResolver(LoginSchema),
-        defaultValues: {
-            correo_electronico: "",
-            password: "",
-        }
-    })
+  const form = useForm<Login>({
+    resolver: zodResolver(LoginSchema),
+    defaultValues: {
+      correo_electronico: "",
+      password: "",
+    },
+  });
 
-    function onSubmit(data: Login) {
-        LoginUser(data);
-    }
+  function onSubmit(data: Login) {
+    LoginUser(data);
+  }
 
-
-    return (
-        <Card className='w-full sm:max-w-md'>
-            <CardHeader>
-                <CardTitle className=''>Iniciar sesión</CardTitle>
-                <CardDescription>
-                    Ingresa tus credenciales
-                </CardDescription>
-            </CardHeader>
-            <CardContent>
-                <form onSubmit={form.handleSubmit(onSubmit)} >
-                    <FieldGroup>
-                        <Controller
-                            name='correo_electronico'
-                            control={form.control}
-                            render={({ field, fieldState }) => (
-                                <Field data-invalid={fieldState.invalid}>
-                                    <FieldLabel htmlFor='correo'>
-                                        Correo Electronico
-                                    </FieldLabel>
-                                    <Input
-                                        {...field}
-                                        id="correo"
-                                        aria-invalid={fieldState.invalid}
-                                        placeholder="Ingresa tu Correo Electronico"
-                                        autoComplete="off"
-                                    />
-                                    {fieldState.invalid && (
-                                        <FieldError errors={[fieldState.error]} />
-                                    )}
-                                </Field>
-                            )}
-                        />
-                        <Controller
-                            name='password'
-                            control={form.control}
-                            render={({ field, fieldState }) => (
-                                <Field data-invalid={fieldState.invalid}>
-                                    <FieldLabel htmlFor='password'>
-                                        Contraseña
-                                    </FieldLabel>
-                                    <Input
-                                        {...field}
-                                        id="password"
-                                        aria-invalid={fieldState.invalid}
-                                        placeholder="Ingresa tu Contraseña"
-                                        type='password'
-                                    />
-                                    {fieldState.invalid && (
-                                        <FieldError errors={[fieldState.error]} />
-                                    )}
-                                </Field>
-                            )}
-                        />
-                    </FieldGroup>
-                    <CardFooter>
-                        <Field orientation="horizontal">
-                            <Button type="submit">
-                                Submit
-                            </Button>
-                        </Field>
-                    </CardFooter>
-                </form>
-            </CardContent>
-
-        </Card>
-    )
+  return (
+    <Card className="w-full sm:max-w-md">
+      <CardHeader>
+        <CardTitle className="">Iniciar sesión</CardTitle>
+        <CardDescription>Ingresa tus credenciales</CardDescription>
+      </CardHeader>
+      <CardContent>
+        <form onSubmit={form.handleSubmit(onSubmit)}>
+          <FieldGroup>
+            <FormInput<Login>
+              name="correo_electronico"
+              control={form.control}
+              label="Correo Electronico"
+              type="text"
+              placeholder="ingresa tu correo electronico"
+            />
+            <FormInput<Login>
+              name="password"
+              control={form.control}
+              label="Contraseña"
+              type="password"
+              placeholder="ingresa tu contraseña"
+            />
+          </FieldGroup>
+          <CardFooter className="mt-6 px-0 pb-0">
+            <Field>
+              <Button type="submit">Submit</Button>
+            </Field>
+          </CardFooter>
+        </form>
+      </CardContent>
+    </Card>
+  );
 }
 
-export default LoginForm
+export default LoginForm;
