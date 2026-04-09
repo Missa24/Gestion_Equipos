37378a607e3c671535c393a03310f8386ad7bcd2
misael <missaelapaza@gmail.com>
2026-04-09 13:35:34 -0400

    configuracion de rutas protegidas

--- src/routes/router.tsx
+++ src/routes/router.tsx
@@ -1,4 +1,4 @@
-import { createBrowserRouter } from "react-router-dom";
+import { createBrowserRouter, Navigate } from "react-router-dom";
 import RootLayout from "@/components/layout/RootLayout";
 import DashboardLayout from "@/components/layout/DashboardLayout";
 import Dashboard from "@/pages/Dashboard";
@@ -6,10 +6,15 @@ import AuthPage from "@/pages/AuthPage";
 import Reportes from "@/pages/ReportesPage";
 import { ProtectedRoute } from "@/components/login/ProtectedRoute";
 import { SupportPage } from "@/pages/SupportPage";
-import { SupportDetailPage } from "@/pages/SupportDetailPage";
-import UsuariosPage from "@/features/Usuarios/Components/UsuariosPage"; // ← U mayúscula
+import { SupportDetailPage } from "@/pages/Support/SupportDetailPage";
+import UsuariosPage from "@/features/Usuarios/Components/UsuariosPage";
 import { EquiposPage } from "@/features/Equipos";
 import { SystemPage } from "@/pages/SystemPage";
+import ServidorPage from "@/pages/ServidorPage";
+import { ServidorDetailPage } from "@/pages/Servidor/ServidorDetailPage";
+import { SystemDetailPage } from "@/pages/System/SystemDetailPage";
+import { DatabasePage } from "@/pages/DatabasePage";
+import { DatabaseDetailPage } from "@/pages/Database/DatabaseDetailPage";
 
 export const router = createBrowserRouter([
   {
@@ -18,10 +23,13 @@ export const router = createBrowserRouter([
     errorElement: <div>Error</div>,
     children: [
       {
+        index: true,
+        element: <Navigate to="/login" replace />,
+      },
+      {
         path: "login",
         element: <AuthPage />,
       },
-
       {
         path: "dashboard",
         element: (
@@ -37,6 +45,11 @@ export const router = createBrowserRouter([
           { path: "usuarios", element: <UsuariosPage /> },
           { path: "equipos", element: <EquiposPage /> },
           { path: "sistemas", element: <SystemPage /> },
+          { path: "sistemas/:id", element: <SystemDetailPage /> },
+          { path: "servidores", element: <ServidorPage /> },
+          { path: "servidores/:id", element: <ServidorDetailPage /> },
+          { path: "base_de_datos", element: <DatabasePage /> },
+          { path: "base_de_datos/:id", element: <DatabaseDetailPage /> },
         ],
       },
     ],
